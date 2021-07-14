de373d6615872ed638ead42f6bde60ad9f77e107
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-07-14 13:35:33 +0700

    commit

--- app/src/main/java/com/gzeinnumer/forceclose/log/ExceptionHandler.java
+++ app/src/main/java/com/gzeinnumer/forceclose/log/ExceptionHandler.java
@@ -6,6 +6,9 @@ import java.io.StringWriter;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
+import android.util.Log;
+
+import com.gzeinnumer.forceclose.BuildConfig;
 
 public class ExceptionHandler implements java.lang.Thread.UncaughtExceptionHandler {
     private final Activity myContext;
@@ -15,7 +18,25 @@ public class ExceptionHandler implements java.lang.Thread.UncaughtExceptionHandl
         myContext = context;
     }
 
+    private void reportFatalCrash(Throwable exception) {
+//        FirebaseApp firebaseApp = FirebaseApp.getInstance();
+//        if (firebaseApp != null) {
+//            try {
+//                FirebaseCrash.getInstance(firebaseApp)
+//                        .zzg(exception); // Reports the exception as fatal.
+//            } catch (com.google.firebase.crash.internal.zzb zzb) {
+//                Log.d(getClass().getSimpleName(), "reportFatalCrash: Internal firebase crash reporting error "+zzb);
+//
+//            } catch (Throwable t) {
+//                Log.d(getClass().getSimpleName(), "Unknown error during firebase crash reporting "+t);
+//            }
+//        } else Log.d(getClass().getSimpleName(), "no FirebaseApp!!");
+    }
+
     public void uncaughtException(Thread thread, Throwable exception) {
+        if (BuildConfig.DEBUG) exception.printStackTrace();
+        reportFatalCrash(exception);
+
         StringWriter stackTrace = new StringWriter();
         exception.printStackTrace(new PrintWriter(stackTrace));
         StringBuilder errorReport = new StringBuilder();
